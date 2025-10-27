f291b00d2f0face3cf68acb29d9b0395b44a1ec7
shwon <kairise1234@gmail.com>
2025-10-27 12:29:33 +0900

    fix: useCheckPassword 수정

--- src/ui/user/data-access-password-check/useCheckPassword.ts
+++ src/ui/user/data-access-password-check/useCheckPassword.ts
@@ -1,30 +1,34 @@
-import useConfirmModal from '@ui/shared/modal/confirm-modal/useConfirmModal'
-import { checkPassword, CheckPasswordResponse } from '@shared/api'
-import { AxiosErrorData, PasswordCheckFormInput } from '@shared/types'
-import { useMutation } from '@tanstack/react-query'
-import { AxiosError } from 'axios'
+/* eslint-disable @typescript-eslint/semi */
+import useConfirmModal from '@ui/shared/modal/confirm-modal/useConfirmModal';
+import { deleteMe } from '@shared/api';
+import { AxiosErrorData, PasswordCheckFormInput } from '@shared/types';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 const useCheckPassword = () => {
-  const { openConfirmModal } = useConfirmModal()
+  const { openConfirmModal } = useConfirmModal();
 
   const mutation = useMutation<
-    CheckPasswordResponse,
+    { message: string },
     AxiosError<AxiosErrorData>,
     PasswordCheckFormInput,
     unknown
   >({
-    mutationFn: async (data) => await checkPassword(data),
+    mutationFn: async ({ password }) => {
+      // 회원탈퇴 API가 비밀번호를 body로 받도록 되어 있으니 이걸 사용
+      return await deleteMe(password);
+    },
     onSuccess: () => {},
     onError: (error) => {
       const text =
-        error?.response?.data?.message || '회원 정보 확인에 실패했습니다.'
+        error?.response?.data?.message || '회원 정보 확인에 실패했습니다.';
       openConfirmModal({
         text,
-      })
+      });
     },
-  })
+  });
 
-  return mutation
-}
+  return mutation;
+};
 
-export default useCheckPassword
+export default useCheckPassword;
